9f4706063b5940909ba76faccd1725f9288d22f5
zany <135144670+Jor3nzo@users.noreply.github.com>
2024-01-31 19:59:50 +0200

    Some

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -5,20 +5,38 @@ using UnityEngine;
 public class Food : MonoBehaviour
 {
     Rigidbody2D rb;
+    public GameObject Pear;
 
     void Start()
     {
+        InvokeRepeating("Spawn", 1f, 3f);
+
         rb = GetComponent<Rigidbody2D>();
 
         rb.velocity = new Vector2(0, 10);
     }
 
+
+
     void Update()
     {
+        
         if (transform.position.y < -6)
         {
             print(":(");
             Destroy(gameObject);
         }    
+    }
+
+    void Spawn()
+    {
+
+        var randPos = new Vector2(Random.Range(-4, 4), Random.Range(-6.3f, -6));
+
+    
+         Instantiate(Pear, randPos, Quaternion.identity);
+        
+
+        
     }
 }
